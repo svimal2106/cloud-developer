3ca505b38460ac30ed0cee3afd38a0fa36fb9762
Vimal Sharma <svimal2106@gmail.com>
2020-11-15 15:33:03 +0530

    Deploying app on ELB

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -6,42 +6,78 @@ import * as AWS from '../../../../aws';
 const router: Router = Router();
 
 // Get all feed items
-router.get('/', async (req: Request, res: Response) => {
+router.get('/', requireAuth, async (req: Request, res: Response) => {
     const items = await FeedItem.findAndCountAll({order: [['id', 'DESC']]});
     items.rows.map((item) => {
-            if(item.url) {
+            if (item.url) {
                 item.url = AWS.getGetSignedUrl(item.url);
             }
     });
     res.send(items);
 });
 
-//@TODO
-//Add an endpoint to GET a specific resource by Primary Key
+router.get('/:id', async(req: Request, res: Response) => {
+    // @ts-ignore
+    const item: FeedItem = await FeedItem.findByPk(req.params['id']);
+    res.send(item);
+});
 
 // update a specific resource
-router.patch('/:id', 
-    requireAuth, 
+router.patch('/:id',
+    requireAuth,
     async (req: Request, res: Response) => {
-        //@TODO try it yourself
-        res.send(500).send("not implemented")
+        const caption = req.body.caption;
+        const url = req.body.url;
+        const existing_item = await FeedItem.findByPk(req.params['id']);
+        existing_item.caption = req.body.caption;
+        existing_item.url = req.body.url;
+        const saved_item = await existing_item.save();
+        res.status(201).send(saved_item);
 });
 
 
+// Delete a specific resource
+router.delete('/:id', async(req: Request, res: Response) => {
+ const id = req.params['id'];
+ const existing_item: FeedItem = await FeedItem.findByPk(id);
+ const deleted_item = existing_item.destroy();
+
+ res.status(201).send('Item with id ' +  id  + ' deleted');
+});
+
+// Batch delete - delete the feed items whose id lie in request json
+router.post('/batchdelete', async(req: Request, res: Response) => {
+    const id_list: number[]  = req.body.ids;
+    for (const id of id_list) {
+        console.log('Id ' + id);
+        const existing_item: FeedItem = await FeedItem.findByPk(id);
+        if (existing_item) {
+            await existing_item.destroy().catch( err =>
+                console.log('Delete item failed with error ' + err)
+            );
+            console.log('Entry for item ' + id + ' deleted');
+        } else {
+            console.log('Entry for item ' + id + ' doesnt exist');
+        }
+    }
+    res.status(201).send('Items with ids ' + id_list + ' deleted');
+} );
+
+
 // Get a signed url to put a new item in the bucket
-router.get('/signed-url/:fileName', 
-    requireAuth, 
+router.get('/signed-url/:fileName',
+    requireAuth,
     async (req: Request, res: Response) => {
-    let { fileName } = req.params;
+    const { fileName } = req.params;
     const url = AWS.getPutSignedUrl(fileName);
     res.status(201).send({url: url});
 });
 
-// Post meta data and the filename after a file is uploaded 
+// Post meta data and the filename after a file is uploaded
 // NOTE the file name is they key name in the s3 bucket.
 // body : {caption: string, fileName: string};
-router.post('/', 
-    requireAuth, 
+router.post('/',
+    requireAuth,
     async (req: Request, res: Response) => {
     const caption = req.body.caption;
     const fileName = req.body.url;
--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -7,41 +7,43 @@ import * as jwt from 'jsonwebtoken';
 import { NextFunction } from 'connect';
 
 import * as EmailValidator from 'email-validator';
+import has = Reflect.has;
 
 const router: Router = Router();
+const numSaltRounds = 10;
+const secret = 'hello';
 
 async function generatePassword(plainTextPassword: string): Promise<string> {
-    //@TODO Use Bcrypt to Generated Salted Hashed Passwords
+    return bcrypt.hash(plainTextPassword, numSaltRounds);
 }
 
 async function comparePasswords(plainTextPassword: string, hash: string): Promise<boolean> {
-    //@TODO Use Bcrypt to Compare your password to your Salted Hashed Password
+    return bcrypt.compare(plainTextPassword, hash);
 }
 
 function generateJWT(user: User): string {
-    //@TODO Use jwt to create a new JWT Payload containing
+    return jwt.sign(user.toJSON(), secret);
 }
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
-    return next();
-    // if (!req.headers || !req.headers.authorization){
-    //     return res.status(401).send({ message: 'No authorization headers.' });
-    // }
+    if (!req.headers || !req.headers.authorization){
+        return res.status(401).send({ message: 'No authorization headers.' });
+    }
     
 
-    // const token_bearer = req.headers.authorization.split(' ');
-    // if(token_bearer.length != 2){
-    //     return res.status(401).send({ message: 'Malformed token.' });
-    // }
+    const token_bearer = req.headers.authorization.split(' ');
+    if(token_bearer.length != 2){
+        return res.status(401).send({ message: 'Malformed token.' });
+    }
     
-    // const token = token_bearer[1];
+    const token = token_bearer[1];
 
-    // return jwt.verify(token, "hello", (err, decoded) => {
-    //   if (err) {
-    //     return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
-    //   }
-    //   return next();
-    // });
+    return jwt.verify(token, secret, (err, decoded) => {
+      if (err) {
+        return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
+      }
+      return next();
+    });
 }
 
 router.get('/verification', 
