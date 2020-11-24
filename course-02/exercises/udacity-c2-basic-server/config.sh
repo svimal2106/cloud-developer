b06bb21558ec71c3d5ef7b5545f05e54da45ea24
Vimal Sharma <svimal2106@gmail.com>
2020-11-24 18:47:39 +0530

    Remove unnecessary files and add secrets file to git ignore

--- course-02/exercises/udacity-c2-basic-server/config.sh
+++
@@ -1,7 +0,0 @@
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=postgres
-export POSTGRES_DB=postgres
-export POSTGRES_HOST=udagramsvimal2106.cudtsta8nbvq.us-east-2.rds.amazonaws.com
-export AWS_REGION=us-east-2
-export AWS_PROFILE=default
-export AWS_BUCKET=udacity-svimal2106
